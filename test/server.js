b107b03cce5f3372148ca25198dd6877c5915e8a
Laurent Christophe <lachrist@vub.ac.be>
2019-03-10 13:45:17 +0100

    npm

--- test/server.js
+++ test/server.js
@@ -6,9 +6,4 @@ const distributor = Distributor((origin, recipient, message) => {
 const server = Net.createServer();
 server.on("connection", distributor.ConnectionListener());
 server.listen(process.argv[process.argv.length-1]);
-setTimeout(() => {
-  server.close();
-}, 2000);
-process.on("exit", () => {
-  console.log("exit");
-});
+setTimeout(() => { server.close() }, 2000);
